83a13fccef527a122f27e7a2541f006e89bde986
FormunaGit <formuna@tuta.io>
2024-12-31 15:25:27 -0400

    Yet another change dump
    I should really improve on pushing to Git scheduling...
    Here's a changelog:
    - Added new .gitignore
    - Added AGS bottom bar
    - Renamed Bar.tsx to Bars.tsx to be more proper with what it contains
    - Lowered Swap size to 4GB
    - Removed Sudo configuration since it didn't do anything...
    - Removed most virtualization stuff
    - Temporarily disabled Weylus config due to it being (hopefully
    temporarily) broken.
    - Removed QEMU, QuickEMU, Motrix, Coreutils-Full, Nautilus, Python312,
    Temurin-JRE, NodeJS, GitHub and Python Rich packages.
    - Added `dart-sass` and Krita packages
    - Added Chaotic-Nyx input in Flake (TODO: Actually use Chaotic-Nyx)
    - Some other shtuff in flake.nix
    - Added some packages for Astal modules
    - Replaced Waybar with AGS in hyprland.conf
    - Copied Launcher, Workspaces, Media, Time and Tray modules from
    @N3RDIUM (thx btw) and made some custom widgets
    - Added Credits in README
    - Moved style.scss

---
+++ dotfiles/ags/widget/Bars.tsx
@@ -0,0 +1,78 @@
+import { App, Astal, Gtk, Gdk } from "astal/gtk3"
+import { bind } from "astal"
+// @ts-ignore
+import Hyprland from "gi://AstalHyprland"
+import { Tray } from "./tray"
+import { Divider } from "./Smolwidgets"
+
+function Workspaces() {
+    const hypr = Hyprland.get_default()
+
+    return <box className="Workspaces">
+        {bind(hypr, "workspaces").as(wss => wss
+            .filter((ws: { id: number }) => !(ws.id >= -99 && ws.id <= -2)) // filter out special workspaces
+            .sort((a: { id: number }, b: { id: number }) => a.id - b.id)
+            .map((ws: { focus: () => unknown; id: any }) => (
+                <button
+                    className={bind(hypr, "focusedWorkspace").as(fw =>
+                        ws === fw ? "focused" : "")}
+                    onClicked={() => ws.focus()}>
+                    {ws.id}
+                </button>
+            ))
+        )}
+    </box>
+}
+
+function FocusedClient() {
+    const hypr = Hyprland.get_default()
+    const focused = bind(hypr, "focusedClient")
+
+    return <box
+        className="Focused"
+        visible={focused.as(Boolean)}>
+        {focused.as(client => (
+            client && <label label={bind(client, "title").as(String)} />
+        ))}
+    </box>
+}
+
+export default function TopBar(gdkmonitor: Gdk.Monitor) {
+    const { TOP, LEFT, RIGHT } = Astal.WindowAnchor
+    return <window className="Top Bar" gdkmonitor={gdkmonitor} exclusivity={Astal.Exclusivity.EXCLUSIVE} anchor={TOP | LEFT | RIGHT} application={App}>
+        <box hexpand>
+            <box halign={Gtk.Align.START}>
+                <Workspaces/>
+            </box>
+            <Divider/>
+
+            <FocusedClient/>
+
+            <Divider/>
+            <box halign={Gtk.Align.END}>
+                <box className="Tray">
+                    <Tray/>
+                </box>
+            </box>
+        </box>
+    </window>
+}
+
+export function BottomBar(gdkmonitor: Gdk.Monitor) {
+    const { BOTTOM, LEFT, RIGHT } = Astal.WindowAnchor
+    return <window className="Bottom Bar" gdkmonitor={gdkmonitor} exclusivity={Astal.Exclusivity.EXCLUSIVE} anchor={BOTTOM | LEFT | RIGHT} application={App}>
+        <box hexpand>
+            <box halign={Gtk.Align.START}>
+                <Workspaces/>
+            </box>
+            <Divider/>
+            <FocusedClient/>
+            <Divider/>
+            <box halign={Gtk.Align.END}>
+                <box className="Tray">
+                    <Tray/>
+                </box>
+            </box>
+        </box>
+    </window>
+}
