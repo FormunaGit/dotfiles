7e2d1f98fbca1129adc03f77a2f00dd740b21772
FormunaGit <formuna@tuta.io>
2025-01-28 18:07:28 -0400

    moved sum stuff :3

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  description = "Formuna's desktop NixOS configuration :D";
+
+  #nixConfig = {
+  #  substituters =
+  #    [ "https://cache.nixos.org" "https://nix-community.cachix.org" ];
+  #  trusted-public-keys = [
+  #    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+  #  ];
+  #};
+
+  inputs = {
+    # NixOS official package source, using the nixos-unstable branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+    ags = {
+      url = "github:Aylur/ags";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    astal = {
+      url = "github:Aylur/astal";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.darwin.follows = "";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, chaotic, agenix, ... }@inputs:
+    let system = "x86_64-linux";
+    in {
+      # Please replace unimag with your hostname
+      nixosConfigurations.unimag = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./System/configuration.nix # The classic configuration.nix file
+          chaotic.nixosModules.default
+          home-manager.nixosModules.home-manager
+          agenix.nixosModules.default
+          {
+            nix.settings.trusted-users = [ "formuna" ];
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.formuna = import ./System/home.nix;
+            home-manager.backupFileExtension = "backup";
+            home-manager.extraSpecialArgs = { inherit inputs; };
+          }
+        ];
+      };
+    };
+}
--- scripts/quicksudos/clean.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-nix-collect-garbage -d
-/run/current-system/bin/switch-to-configuration boot
