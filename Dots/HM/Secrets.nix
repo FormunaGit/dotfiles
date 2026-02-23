8a89e0298b760f6e92f0361d940857b494d31643
FormunaGit <formuna@tuta.io>
2026-02-23 18:39:40 -0400

    Fix sops?

--- Dots/HM/Secrets.nix
+++ Dots/HM/Secrets.nix
@@ -1,9 +1,13 @@
 { ... }:
+let
+  keyPath = "/run/user/1000/secrets/";
+in
 {
   sops = {
     age.keyFile = "/home/formuna/.config/sops/age/keys.txt"; # DON'T SHARE THESE KEYS!!
     defaultSopsFile = ../secrets.json; # Secrets file, use `sops edit` to edit.
     defaultSymlinkPath = "/run/user/1000/secrets"; # Secrets path.
     defaultSecretsMountPoint = "/run/user/1000/secrets.d"; # Secrets path 2.
+    secrets.email.path = keyPath + "email";
   };
 }
